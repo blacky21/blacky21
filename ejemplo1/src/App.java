aa1d8a6cb65565e04eb1a1e80825095f08db23aa
Sergio Sánchez Mena <84925894+blacky21@users.noreply.github.com>
2022-09-12 20:59:13 -0500

    Petición de tareas

--- ejemplo1/src/App.java
+++ ejemplo1/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mundo!");
-        System.out.println ("Hola Chetitos")
+        System.out.println ("Hola Chetitos");
+        System.out.println ("Ya entreguen sus tareas");
     }
 }
