20c06b7bf3fdd6563537313205d3bb7232cbd931
blacky21 <blackzabath88@gmail.com>
2022-09-12 21:38:16 -0500

    Segunda versión

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola mundo!");
+        System.out.println("Hola Sergio");
+        System.out.println("Saludos al chetos mayor juan");
+    }
+}
