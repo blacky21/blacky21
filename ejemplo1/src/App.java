b694d3daad4c2dfca691e68a02e0f771d0e86687
Sergio Sánchez Mena <84925894+blacky21@users.noreply.github.com>
2022-09-12 21:09:55 -0500

    Se agregó el apellido a la línea

--- ejemplo1/src/App.java
+++ ejemplo1/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mundo!");
-        System.out.println("Hola Sergio");
+        System.out.println("Hola Sergio Sánchez Mena"); 
     }
 }
